908670cae052571fdfcfeffa71af08b11f7c3288
Wesley Benvindo <wes.bnvnd@gmail.com>
2021-03-24 22:22:17 -0300

    list all ok

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,13 +6,12 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { id } = request.headers;
+    const { user_id } = request.headers;
 
-    const idToString = String(id);
+    const idToString = String(user_id);
 
     try {
       const all = this.listAllUsersUseCase.execute({ user_id: idToString });
-
       return response.json(all);
     } catch (error) {
       return response.status(400).json({ error: error.message });
